6621b3c9a608ce4f5b8a78237baca9e49aad43e8
m30164 <m.c.j.bakker@student.tue.nl>
2015-02-19 23:41:31 +0100

    Revert "Advanced Unit Selection"
    This reverts commit 5c472e8f9d1967ec06a98c0d5269c5620d53e742.

--- public_html/config.js
+++ public_html/config.js
@@ -10,12 +10,7 @@
 // This controls the units used in the plane table,
 // and whether metric or imperial units are shown first
 // in the detailed plane info.
-// You can enable either Metric or Imperial, or even both.
-// When you select both, you can set your preference with the preferMetric variable.
-PreferMetric = true;
-EnableMetric = true;
-EnableImperial = false;
-
+Metric = false; // true or false
 
 // -- Map settings ----------------------------------------
 // These settings are overridden by any position information
